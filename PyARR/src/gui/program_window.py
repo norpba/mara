bd21521c6dcc6f9cf93c22b0385b5e08f9879720
norppa <108995411+norpba@users.noreply.github.com>
2023-10-28 22:04:34 +0300

    Worked on the code for the gui and added some docs + demos

--- PyARR/src/gui/program_window.py
+++ PyARR/src/gui/program_window.py
@@ -1,8 +1,27 @@
-# PaRR - Python Arranger
+# PyARR - Python Arranger
 
 import PySimpleGUI as sg
 
 # Welcome-window
+def welcome_window():
 
-welcomewindow_layout = [[sg.Text("Welcome to Python Arranger v0.1.0 !")], [sg.Button("Ok", expand_y=True)]]
-sg.Window(title="Python Arranger v0.1.0", layout = welcomewindow_layout, margins=(0, 15)).read()
+    layout = [[sg.Text('Welcome to Python Arranger!', justification='center',size=(100,1))]
+              ]
+    
+    button = [
+        [[sg.Button('Ok',size=(100,1))]]
+        ]
+
+    window = sg.Window("PyARR v0.1.0", layout, button, size=(400, 100))
+
+    while True:
+        event, values = window.read()
+        print(event, values)
+        if event == None or event == button:
+            break
+        if event == 'Button':
+            break
+        window.close()
+if __name__ == "__main__":
+    welcome_window()
+        #main()
